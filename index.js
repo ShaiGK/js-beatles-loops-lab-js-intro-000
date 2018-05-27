7d4c742de7f476cb7fbc073a13a6b4f9d957f22e
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-05-27 20:18:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay (musician, instrument) {
+  var array = []
+  for (var i = 0; i < 5; i++) {
+    array = array[musician [i] + " plays " + instrument [i]]
+  }
+  return array
+}
