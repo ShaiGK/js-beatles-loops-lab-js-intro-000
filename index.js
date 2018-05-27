383040a7bd3215811bed1d345b4426f9af958627
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-05-27 20:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function theBeatlesPlay (m,i) {
-  for (var x = 0; x > 5; x++) {
-    return (m[x] + " plays " + i[x])
+function theBeatlesPlay (people,things) {
+  for (var i = 0; i > 5; i++) {
+    return (people[i] + " plays " + things[i])
   }
 }
