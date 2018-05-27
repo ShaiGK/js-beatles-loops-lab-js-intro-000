deaca6c630a1498aa327733e15b6b3260735b759
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-05-27 21:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay (musicians,instruments) {
 
 function johnLennonFacts (facts) {
   var i = 0
-  for (i < truths.length) {
+  while (i < truths.length) {
     facts[i] = `${facts[i]}!!!`
     i++
   }
